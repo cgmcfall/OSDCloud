79abfdd651346478e67454516428a4a0fb78aba7
Chris McFall <chris@mcfall.co.uk>
2025-02-05 17:27:13 +0100

    Adding params to BuildMain.ps1

--- BuildMain.ps1
+++ BuildMain.ps1
@@ -10,7 +10,7 @@ New-OSDCloudWorkspace -WorkspacePath C:\OSDCloud
  
 New-OSDCloudUSB
  
-Edit-OSDCloudwinPE -workspacepath C:\OSDCloud -CloudDriver * -WebPSScript https://gist.githubusercontent.com/ -Verbose
+Edit-OSDCloudwinPE -workspacepath C:\OSDCloud -CloudDriver * -WebPSScript https://gist.github.com/cgmcfall/7c75e799e7fb9e8b58e10885efdaaff2 -Verbose
  
 New-OSDCloudISO
  
